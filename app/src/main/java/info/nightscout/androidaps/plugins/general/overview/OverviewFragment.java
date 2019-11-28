b9138fb474e35d0b7c4324e379148bec1e15f722
Richard <chuma@live.de>
2019-11-28 12:57:49 +0100

    Modified

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.java
@@ -1311,16 +1311,16 @@ public class OverviewFragment extends Fragment implements View.OnClickListener, 
 //                        + MainApp.gs(R.string.basal) + ": " + DecimalFormatter.to2Decimal(basalIob.basaliob) + "U\n";
 //                OKDialog.show(getActivity(), MainApp.gs(R.string.iob), iobtext1, null);
 //            });
-        } else if (MainApp.sResources.getBoolean(R.bool.isTablet)) {
-            String iobtext = DecimalFormatter.to2Decimal(bolusIob.iob + basalIob.basaliob) + "U ("
-                    + MainApp.gs(R.string.bolus) + ": " + DecimalFormatter.to2Decimal(bolusIob.iob) + "U "
-                    + MainApp.gs(R.string.basal) + ": " + DecimalFormatter.to2Decimal(basalIob.basaliob) + "U)";
-            iobView.setText(iobtext);
-        } else {
-            String iobtext = DecimalFormatter.to2Decimal(bolusIob.iob + basalIob.basaliob) + "U ("
-                    + DecimalFormatter.to2Decimal(bolusIob.iob) + "/"
-                    + DecimalFormatter.to2Decimal(basalIob.basaliob) + ")";
-            iobView.setText(iobtext);
+//        } else if (MainApp.sResources.getBoolean(R.bool.isTablet)) {
+//            String iobtext = DecimalFormatter.to2Decimal(bolusIob.iob + basalIob.basaliob) + "U ("
+//                    + MainApp.gs(R.string.bolus) + ": " + DecimalFormatter.to2Decimal(bolusIob.iob) + "U "
+//                    + MainApp.gs(R.string.basal) + ": " + DecimalFormatter.to2Decimal(basalIob.basaliob) + "U)";
+//            iobView.setText(iobtext);
+//        } else {
+//            String iobtext = DecimalFormatter.to2Decimal(bolusIob.iob + basalIob.basaliob) + "U ("
+//                    + DecimalFormatter.to2Decimal(bolusIob.iob) + "/"
+//                    + DecimalFormatter.to2Decimal(basalIob.basaliob) + ")";
+//            iobView.setText(iobtext);
         }
 
         // cob
