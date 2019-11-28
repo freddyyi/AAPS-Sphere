062372f10f5ac1fea19ebcb09dc97b5eec1353fe
Richard <chuma@live.de>
2019-11-28 13:18:12 +0100

    Modified

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.java
@@ -1305,12 +1305,12 @@ public class OverviewFragment extends Fragment implements View.OnClickListener, 
         if (shorttextmode) {
             String iobtext = DecimalFormatter.to2Decimal(bolusIob.iob + basalIob.basaliob) + "U";
             iobView.setText(iobtext);
-            iobView.setOnClickListener(v -> {
-                String iobtext1 = DecimalFormatter.to2Decimal(bolusIob.iob + basalIob.basaliob) + "U\n";
+//            iobView.setOnClickListener(v -> {
+//                String iobtext1 = DecimalFormatter.to2Decimal(bolusIob.iob + basalIob.basaliob) + "U\n";
 //                        + MainApp.gs(R.string.bolus) + ": " + DecimalFormatter.to2Decimal(bolusIob.iob) + "U\n"
 //                        + MainApp.gs(R.string.basal) + ": " + DecimalFormatter.to2Decimal(basalIob.basaliob) + "U\n";
-                OKDialog.show(getActivity(), MainApp.gs(R.string.iob), iobtext1, null);
-            });
+//                OKDialog.show(getActivity(), MainApp.gs(R.string.iob), iobtext1, null);
+//        });
         } else if (MainApp.sResources.getBoolean(R.bool.isTablet)) {
             String iobtext = DecimalFormatter.to2Decimal(bolusIob.iob + basalIob.basaliob) + "U ("
                     + MainApp.gs(R.string.bolus) + ": " + DecimalFormatter.to2Decimal(bolusIob.iob) + "U "
