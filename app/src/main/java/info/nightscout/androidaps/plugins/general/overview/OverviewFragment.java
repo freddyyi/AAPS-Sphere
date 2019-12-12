e9e2e2382d7a603ad16cafe8efa361796a235fd8
Richard <chuma@live.de>
2019-12-12 09:59:38 +0100

    Modified

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/OverviewFragment.java
@@ -1329,7 +1329,7 @@ public class OverviewFragment extends Fragment implements View.OnClickListener, 
         if (timeAgoView != null)
             timeAgoView.setText(DateUtil.minAgo(lastBG.date));
         if (timeAgoShortView != null)
-            timeAgoShortView.setText( DateUtil.minAgo(lastBG.date) + "′");
+            timeAgoShortView.setText( DateUtil.minAgoShort(lastBG.date) + "′");
 
         // iob
         TreatmentsPlugin.getPlugin().updateTotalIOBTreatments();
