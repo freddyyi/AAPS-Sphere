21368d3a830b1eb759b9be0acea7efb38bbb1603
Richard <chuma@live.de>
2019-12-13 16:42:51 +0100

    Modified

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/StatuslightHandler.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/StatuslightHandler.java
@@ -79,6 +79,7 @@ class StatuslightHandler {
                 (Double threshold) -> value <= threshold;
         if (value != invalid) {
             view.setText(text);
+            view.getBackground();
             if (check.apply(urgentThreshold)) {
                 Drawable drawable = applyStatuslight().getBackground();
                 drawable.setColorFilter(new PorterDuffColorFilter(0xffE0191D, PorterDuff.Mode.SRC_OUT));
