6b72c75c5b3aa794c6c64078f6f3ad56fd96424d
Richard <chuma@live.de>
2019-12-15 15:56:45 +0100

    Change Statuslight Look

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/StatuslightHandler.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/StatuslightHandler.java
@@ -116,7 +116,7 @@ class StatuslightHandler {
 
         handleLevel(R.string.key_statuslights_res_critical, 20.0,
                 R.string.key_statuslights_res_warning, 50.0,
-                reservoirView, " U ", pump.getReservoirLevel());
+                reservoirView, " U", pump.getReservoirLevel());
 
         handleAge("sage", CareportalEvent.SENSORCHANGE, sageView, "",
                 164, 166);
@@ -126,7 +126,7 @@ class StatuslightHandler {
                     R.string.key_statuslights_bat_warning, 51.0,
                     batteryView, " ", pump.getBatteryLevel());
         } else {
-            handleAge("bage", CareportalEvent.PUMPBATTERYCHANGE, batteryView, " % ",
+            handleAge("bage", CareportalEvent.PUMPBATTERYCHANGE, batteryView, " |% ",
                     336, 240);
         }
     }
