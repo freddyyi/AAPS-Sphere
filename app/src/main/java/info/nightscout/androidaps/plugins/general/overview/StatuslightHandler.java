444b4ed79fb9a9e18bf4ac069135c40163577eef
Richard <chuma@live.de>
2019-12-15 17:20:21 +0100

    Change Statuslight Look

--- app/src/main/java/info/nightscout/androidaps/plugins/general/overview/StatuslightHandler.java
+++ app/src/main/java/info/nightscout/androidaps/plugins/general/overview/StatuslightHandler.java
@@ -81,15 +81,15 @@ class StatuslightHandler {
             view.setText(text);
 //            view.setBackgroundColor(MainApp.gc(R.color.transparent));
             if (check.apply(urgentThreshold)) {
-                view.setTextColor(MainApp.gc(R.color.color_white));
+                view.setTextColor(MainApp.gc(R.color.white));
                 Drawable drawable = view.getBackground();
                 drawable.setColorFilter(new PorterDuffColorFilter(0xffcd6839, PorterDuff.Mode.SRC_OUT));
             } else if (check.apply(warnThreshold)) {
-                view.setTextColor(MainApp.gc(R.color.color_white));
+                view.setTextColor(MainApp.gc(R.color.white));
                 Drawable drawable = view.getBackground();
                 drawable.setColorFilter(new PorterDuffColorFilter(0xfff0a30a, PorterDuff.Mode.SRC_OUT));
             } else {
-                view.setTextColor(MainApp.gc(R.color.color_white));
+                view.setTextColor(MainApp.gc(R.color.white));
                 Drawable drawable = view.getBackground();
                 drawable.setColorFilter(new PorterDuffColorFilter(0x20FFFFFF, PorterDuff.Mode.SRC_OUT));
             }
